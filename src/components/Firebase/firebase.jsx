a3afc05de6aede83128ff3cd360316c453451fe5
Harshith <97115515+h4r5h1th@users.noreply.github.com>
2023-06-14 18:30:48 +0530

    Update firebase.jsx

--- src/components/Firebase/firebase.jsx
+++ src/components/Firebase/firebase.jsx
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCGf42_XoaGJXeobmuHAo9QYqPgqFzb0sk",
-    authDomain: "twitter-clone-500c2.firebaseapp.com",
-    projectId: "twitter-clone-500c2",
-    storageBucket: "twitter-clone-500c2.appspot.com",
-    messagingSenderId: "349404868612",
-    appId: "1:349404868612:web:2c7d8652aca20f1e880551",
-    measurementId: "G-BT4T2G2MPS"
+    apiKey: "xxxxx",
+    authDomain: "xxxxx",
+    projectId: "xxxxx",
+    storageBucket: "xxxxx",
+    messagingSenderId: "xxxxx",
+    appId: "xxxxx",
+    measurementId: "xxxxx"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
